1075a45507ea30ff6a2b89b594f1ba1805501fb2
Paras Shrestha <55276059+thefunkydude@users.noreply.github.com>
2022-03-05 15:46:51 +0545

    Update Alphabet(O).cpp

--- OpenGL/Alphabet(O).cpp
+++ OpenGL/Alphabet(O).cpp
@@ -1,13 +1,6 @@
 #include <GL/glut.h>
 #include <stdlib.h>
 GLfloat T = 0;
-// void Spin()
-// {
-// 	T = T + 0.03;
-// 	if(T>360)
-// 	T = 0;
-// 	glutPostRedisplay();
-// }
 void MyInit() {
 	glClearColor(0, 0, 0, 1);
 	glColor3f(1, 0, 0);
@@ -27,15 +20,15 @@ void Cube(GLfloat V0[], GLfloat V1[], GLfloat V2[], GLfloat V3[], GLfloat V4[], 
 	glColor3f(0.6, 0, 0.6);
 	Face(V0, V1, V2, V3);//back
 	glColor3f(0.5, 0.9, 0.5);
-	Face(V4, V5, V6, V7);//front
+	Face(V4, V5, V6, V7);
 	glColor3f(1, 0.4, 0);
-	Face(V0, V3, V7, V4);//left
+	Face(V0, V3, V7, V4);
 	glColor3f(1, 0.5, 1);
-	Face(V1, V2, V6, V5);//right
+	Face(V1, V2, V6, V5);
 	glColor3f(0.5, 0.6, 1);
-	Face(V0, V1, V5, V4);//top
+	Face(V0, V1, V5, V4);
 	glColor3f(0.5, 0.4, 1);
-	Face(V2, V3, V7, V6);//bottom
+	Face(V2, V3, V7, V6);
 }
 void Draw() {
 	GLfloat V[8][3] = { {-0.2, 0.2, 0.3},
@@ -86,13 +79,11 @@ void Draw() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glLoadIdentity();
-	//glRotatef(T,1,1,1);//for spinning
-	glRotatef(3, 1, 1, -1);//for stationary
+	glRotatef(3, 1, 1, -1);
 	Cube(V[0], V[1], V[2], V[3], V[4], V[5], V[6], V[7]);
 	Cube(V1[0], V1[1], V1[2], V1[3], V1[4], V1[5], V1[6], V1[7]);
 	Cube(V2[0], V2[1], V2[2], V2[3], V2[4], V2[5], V2[6], V2[7]);
 	Cube(V3[0], V3[1], V3[2], V3[3], V3[4], V3[5], V3[6], V3[7]);
-	//Cube(V4[0], V4[1], V4[2], V4[3], V4[4], V4[5], V4[6], V4[7]);
 
 	glutSwapBuffers();
 }
@@ -107,7 +98,6 @@ int main(int C, char* V[])
 	glutCreateWindow("PRATIMA");
 	MyInit();
 	glutDisplayFunc(Draw);
-	//glutIdleFunc(Spin); //spinning
 	glutMainLoop();
 
 	return 0;
